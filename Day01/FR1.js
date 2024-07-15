6d1ae75bce8d59e0b4df678682aa3ebf860537c5
Rama-chaturvedi <2005krama@gmail.com>
2024-07-15 16:59:56 +0530

    Day01(Feature Request)

---
+++ Day01/FR1.js
@@ -0,0 +1,11 @@
+let num = 45;
+let string = "Rama";
+let boolean = true;
+let object = {name: "Rama", age: 20};
+let myArray = [4, 5, 6, 7]
+
+console.log(typeof num, num);
+console.log(typeof string, string);
+console.log(typeof boolean, boolean);
+console.log(typeof object, object);
+console.log(typeof myArray, myArray);
