83acaaf523d5cdfd0f57d1546408554b8438a0c6
Rama-chaturvedi <2005krama@gmail.com>
2024-07-15 17:21:34 +0530

    Day01(feature Request)

---
+++ Day01/FR2.js
@@ -0,0 +1,17 @@
+// Using let
+let x = 10;
+console.log(x); // Output: 10
+
+x = 20;
+console.log(x); // Output: 20
+
+// Using const
+const y = 10;
+console.log(y); // Output: 10
+
+try {
+  y = 20; // This will throw an error
+} catch (error) {
+  console.error(error); // Output: Assignment to constant variable.
+}
+
