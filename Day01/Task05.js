68610c2c46ce19a7b5430371e6e1a1ea7c461268
Rama-chaturvedi <2005krama@gmail.com>
2024-07-15 16:30:03 +0530

    day01(Reassigning variable)

---
+++ Day01/Task05.js
@@ -0,0 +1,8 @@
+let num = 30;   //declare a variable and assign the value
+console.log(num);
+
+num = "welcome to javascript challenge";  //reassign the value
+console.log(num);
+
+// let num =40;     //(invalid) not allow re-declareation in 'let' keyword. 
+// console.log(num);
