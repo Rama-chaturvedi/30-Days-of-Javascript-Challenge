b846f3c84d814782595c7fa8e64b78b568063d86
Rama-chaturvedi <2005krama@gmail.com>
2024-07-15 15:11:00 +0530

    var keyword

--- Day01/Task01.js
+++ Day01/Task01.js
@@ -7,6 +7,7 @@ console.log(num);
 num = "Task 01"             // assign a new value, without using 'var'  
 console.log(num);
 
+
 // Notes : 
 // 1. We can re-declare and re-assign the value of a variable multiple times
 // 2. We can also assign a new value to a variable without using 'var' keyword, for updating the existing variable.
