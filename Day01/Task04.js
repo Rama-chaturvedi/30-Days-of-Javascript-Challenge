cb4877407f3a2368ae34f136d00c79e7aa889b24
Rama-chaturvedi <2005krama@gmail.com>
2024-07-15 16:14:55 +0530

    Day01(Data Types)

---
+++ Day01/Task04.js
@@ -0,0 +1,25 @@
+// variable declaration for 'number' datatype 
+const id_No = 23;  
+console.log(typeof id_No, id_No);
+
+// variable declaration for 'string' datatype 
+let name = "Rama";
+console.log(typeof name, name);
+
+// variable declaration for 'boolean' datatype 
+let boolean = true;
+console.log(typeof boolean, boolean);
+
+// variable declaration for 'object' datatype 
+let person = {
+    name: "Rama",
+    age: 20,
+    occupation: "frontend developer"
+}
+console.log(typeof person, person);
+
+//variable declaration for 'array' datatype 
+let myArray = [1, 2, 3, 4, 5];
+console.log(typeof myArray, myArray);  
+
+// Note: typeof operator returns "object" for arrays, because arrays are a type of object in JavaScript.
