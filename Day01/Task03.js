945b0437808157f3f0a14b284c5a6267b6548a64
Rama-chaturvedi <2005krama@gmail.com>
2024-07-15 15:28:55 +0530

    Day01(const keyword)

---
+++ Day01/Task03.js
@@ -0,0 +1,7 @@
+const boolean = true;     //declare a variable and assign the value 
+console.log(boolean);
+
+
+// Note: Using 'const' keyword, we can not re-declare variable and re-assign value. 
+
+
