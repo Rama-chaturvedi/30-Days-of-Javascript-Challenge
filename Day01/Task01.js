53da2cc719c8702131fadbdcec35dfb212d6b99e
Rama-chaturvedi <2005krama@gmail.com>
2024-07-15 15:08:35 +0530

    Day01 (let keyword)

--- Day01/Task01.js
+++ Day01/Task01.js
@@ -1,8 +1,13 @@
-var num = 23;
-console.log(num);
+var num = 23;                  //declare a variable num and assign the value     
+console.log(num);              // log the value of variable to the console
 
-var num = "Day 1 challenge"       // Re-assign value 
+var num = "Day 1 challenge"       // Re-declare and Re-assign the value 
 console.log(num);                 
 
-num = "Task 01"
+num = "Task 01"             // assign a new value, without using 'var'  
 console.log(num);
+
+// Notes : 
+// 1. We can re-declare and re-assign the value of a variable multiple times
+// 2. We can also assign a new value to a variable without using 'var' keyword, for updating the existing variable.
+ 
---
+++ Day01/Task02.js
@@ -0,0 +1,9 @@
+let num = "Task 02";  // Declare and assign the value using 'let' keyword
+console.log(num);     // logs the value of variable of the console
+
+num = "30-Days of javascript challenge";        //   reassigns the variable
+console.log(num);  
+
+// note:
+// Using 'let' keyword we declare, assign and re-assign the value.
+// but we cann't re-declare the variable. 
